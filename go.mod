90c22ca2a84c9c4880b0137cc4ed73be2cf26154
liubo04 <liubo04@work.com>
2020-10-10 11:16:45 +0800

    测试例子

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vyzigold/loki-client
+
+go 1.15
--- main.go
+++ main.go
@@ -7,29 +7,32 @@ import (
     "github.com/vyzigold/loki-client/pkg/loki"
 )
 
+var count int = 0
 func sendSomething(client *loki.LokiClient) {
+    count++
     labels := make(map[string]string)
     labels["labelkey"]="labelvalue"
+    labels["level"]=strconv.Itoa(count%4)
     message1 := loki.Message {
         Time: strconv.FormatInt(time.Now().UnixNano(), 10),
         Message: "{\"simple\": [\"json\", \"string\"]}",
     }
     message2 := loki.Message {
         Time: strconv.FormatInt(time.Now().UnixNano(), 10),
-        Message: "not json message",
+        Message: "not json message, cnt=" + strconv.Itoa(count) + ", level=" + strconv.Itoa(count%4),
     }
     messages := []loki.Message{message1, message2}
     client.AddStream(labels, messages)
 }
 
 func main () {
-    client, err := loki.CreateClient("http://localhost:3100", 4, 1000000000)
+    client, err := loki.CreateClient("http://10.62.117.69:3100", 4, 1000000000)
     if err == nil {
         fmt.Println("Client successfuly created")
     }
-    for i := 10; i > 0; i-- {
+    for i := 100000000000; i > 0; i-- {
         sendSomething(client)
-        time.Sleep(100000000)
+        time.Sleep(time.Second)
     }
     response, err := client.Query("{labelkey=~\"lab.*\"}")
     fmt.Println("\nthe response of the query currently is:")
